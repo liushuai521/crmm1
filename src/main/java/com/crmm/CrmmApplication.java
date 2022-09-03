a51f52efa684beaff36a5e367fcef0da005491a5
liushuai <986577977@qq.com>
2022-09-03 15:40:31 +0800

    0903配置环境

---
+++ src/main/java/com/crmm/CrmmApplication.java
@@ -0,0 +1,13 @@
+package com.crmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crmm/CrmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
