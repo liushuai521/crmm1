3de1f64ab7b7de6050f0855571a4310fa88ecc7c
liushuai <986577977@qq.com>
2022-09-03 15:59:50 +0800

    0903配置环境2pojo

---
+++ src/main/java/com/crmm/pojo/Worker.java
@@ -0,0 +1,21 @@
+package com.crmm.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("worker")
+public class Worker {
+    @TableId("wid")
+    private int wid;
+    private String wname;
+    private String wsex;
+    private String wtel;
+    private int deptid;
+    private String wimg;
+}
